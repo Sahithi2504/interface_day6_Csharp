10c54d071baed67b09a51ca99d65e8ac153750b5
M Sahithi <msahithi254@gmail.com>
2022-03-07 12:56:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    internal class Program
+    {
+        interface Drawable
+        {
+            void draw();
+        }
+        class Rectangle : Drawable
+        {
+            public void draw()
+            {
+                Console.WriteLine("drawing rectangle...");
+            }
+        }
+        class Circle : Drawable
+        {
+            public void draw()
+            {
+                Console.WriteLine("drawing circle...");
+            }
+        }
+        class TestInterface
+        {
+            public static void Main()
+            {
+                Drawable d;
+                d = new Rectangle();
+                d.draw();
+                d = new Circle();
+                d.draw();
+                Console.ReadLine();
+            }
+        }
+    }
+}
